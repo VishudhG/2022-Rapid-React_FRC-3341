8d4210b3de0a008ba675325e1ede1609706c644f
VishudhG <90531482+VishudhG@users.noreply.github.com>
2022-01-22 14:52:05 -0800

    Ball Handler Setup 2
    Added specific names, a victor follower motor and a roller motor, and methods to control and get data on them.

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/subsystems/BallHandler.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/subsystems/BallHandler.java
@@ -8,40 +8,80 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
 import frc.robot.Constants;
 
 public class BallHandler extends SubsystemBase {
 
-  private final WPI_TalonSRX motor1 = new WPI_TalonSRX(Constants.MotorPorts.port1);
-  private final WPI_TalonSRX motor2 = new WPI_TalonSRX(Constants.MotorPorts.port1);
-  private final WPI_TalonSRX motor3 = new WPI_TalonSRX(Constants.MotorPorts.port1);
-  private final WPI_TalonSRX motor4 = new WPI_TalonSRX(Constants.MotorPorts.port1);
-  private final Servo servo = new Servo(Constants.MotorPorts.port2);
+  //change ports when ready to start testing
+  private final WPI_TalonSRX leftflywheel = new WPI_TalonSRX(Constants.MotorPorts.port1);
+  private final WPI_TalonSRX rightflywheel = new WPI_TalonSRX(Constants.MotorPorts.port2);
+  private final WPI_TalonSRX pivot = new WPI_TalonSRX(Constants.MotorPorts.port3);
+  private final WPI_VictorSPX pivotfollower = new WPI_VictorSPX(Constants.MotorPorts.port4);
+  private final WPI_VictorSPX roller = new WPI_VictorSPX(Constants.MotorPorts.port5);
+
+  private double ticksToMeters;
+  private double ticksToDegrees;
 
 
   /** Creates a new ExampleSubsystem. */
   public BallHandler() {
-      motor1.configFactoryDefault();
-      motor1.setInverted(false);
-      motor2.configFactoryDefault();
-      motor2.setInverted(false);
-      motor3.configFactoryDefault();
-      motor3.setInverted(false);
-      motor4.configFactoryDefault();
-      motor4.setInverted(false);
+    pivot.configFactoryDefault();
+    pivot.setInverted(false);
+    pivot.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+    roller.configFactoryDefault();
+    roller.setInverted(false);
+    pivotfollower.configFactoryDefault();
+    pivotfollower.follow(pivot);
+    pivotfollower.setInverted(InvertType.FollowMaster);
+    leftflywheel.configFactoryDefault();
+    leftflywheel.setInverted(false);
+    leftflywheel.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
+    rightflywheel.configFactoryDefault();
+    rightflywheel.setInverted(true);
+    rightflywheel.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute);
   
 
+  }
+
+  public void resetEncoders(){
+    leftflywheel.setSelectedSensorPosition(0,0,10);
+    rightflywheel.setSelectedSensorPosition(0,0,10);
+    pivot.setSelectedSensorPosition(0,0,10);
+  }
+
+  public double getFlywheelPosition(){
+    return (((leftflywheel.getSelectedSensorPosition(0) + rightflywheel.getSelectedSensorPosition(0))/2) * (ticksToMeters));
+  }
+
+  public double getPivotPosition(){
+    return (((pivot.getSelectedSensorPosition(0) * (ticksToDegrees))));
+  }
+
+  public double getVelocity(){
+    return (((leftflywheel.getSensorCollection().getPulseWidthVelocity() + rightflywheel.getSensorCollection().getPulseWidthVelocity())/2) * (ticksToMeters));
+ }
+
+  public void setFlywheelPower(double speed) {
+    leftflywheel.set(speed);
+    rightflywheel.set(speed);
+  }
+  
+  public void setPivotPower(double speed) {
+    pivot.set(speed);
+  }
+
+  public void setVictorPower(WPI_VictorSPX motor, double speed) {
+    motor.set(speed);
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
