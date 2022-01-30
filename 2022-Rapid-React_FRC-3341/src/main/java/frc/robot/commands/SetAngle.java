e3aadf78a5bcfc6af2b2152a8fa72e57c1289a73
VishudhG <90531482+VishudhG@users.noreply.github.com>
2022-01-29 16:29:39 -0800

    SetAngle
    Started on the basics for the SetAngle command.

--- 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/SetAngle.java
+++ 2022-Rapid-React_FRC-3341/src/main/java/frc/robot/commands/SetAngle.java
@@ -6,27 +6,38 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.BallHandler;
-import frc.robot.Constants;
 
-public class Intake extends CommandBase {
+public class SetAngle extends CommandBase {
+  /** Creates a new SetAngle. */
+  private double angle;
+  private BallHandler ballhandler;
 
-  private double power;
-
-  public Intake() {
+  public SetAngle(double angle, BallHandler ballHandler) {
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(ballHandler);
+    this.ballhandler = ballHandler;
+    this.angle = angle;
+
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    ballhandler.resetPivotEncoders();
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    ballhandler.setPivotPositionPID(angle);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    
+  }
 
   // Returns true when the command should end.
   @Override
